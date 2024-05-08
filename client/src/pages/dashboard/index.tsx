17093959f123454a45e466ca14c68fe67cf83d5d
Shaurya Jha <76627647+Shaurya-Jha@users.noreply.github.com>
2024-05-09 00:46:57 +0530

    finance dashboard

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Text, Heading } from '@chakra-ui/react'
+import { useUser } from '@clerk/clerk-react'
+import FinancialRecordForm from './financial-record-form'
+import FinancialRecordList from './financial-record-list'
 
 function Dashboard() {
-  return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-  )
+
+    // clerk useUser initialization
+    const { user } = useUser()
+
+    return (
+        <div>
+
+            <div className='flex justify-center items-center w-full'>
+                <Heading as={"h3"}>Welcome {user?.firstName}! Here are your Finances</Heading>
+            </div>
+
+            <div className='grid grid-cols-2'>
+                {/* financial record form */}
+                <div className='bg-white'>
+                    <div className='m-1 rounded-lg bg-gray-200 min-h-screen py-4'>
+                        <FinancialRecordForm />
+                    </div>
+                </div>
+
+                {/* financial record list */}
+                <div className='bg-white'>
+                    <div className='m-1 rounded-lg bg-gray-200 min-h-screen py-4'>
+                        <FinancialRecordList />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard
