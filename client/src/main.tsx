f4ee2e9ac2b7d52b32a446266645a35de284fb00
Shaurya Jha <76627647+Shaurya-Jha@users.noreply.github.com>
2024-05-08 23:59:01 +0530

    added providers for clerk and chakra UI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ClerkProvider } from "@clerk/clerk-react"
+
+// clerk key
+const PUBLiSHABLE_KEY = import .meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLiSHABLE_KEY) {
+  throw new Error("Missing Publishable key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ClerkProvider publishableKey={PUBLiSHABLE_KEY}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ClerkProvider>
   </React.StrictMode>,
 )
