4b32ee63a184e4f3e4382fd57b22b9b21041cd98
Shaurya Jha <76627647+Shaurya-Jha@users.noreply.github.com>
2024-05-10 01:01:22 +0530

    added contextProvider for dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import Auth from './pages/auth'
+import { FinancialRecordsProvider } from './context/financial-record-context'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Dashboard /> } />
+          <Route path='/' element={<FinancialRecordsProvider><Dashboard /></FinancialRecordsProvider>} />
           <Route path='/auth' element={<Auth />} />
         </Routes>
       </Router>
